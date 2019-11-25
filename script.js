1b4e5b5a1f6d20baebad71add5257167916c9e6c
EVAN PATRICK WHELAN <u180539@CS4-43>
2019-11-25 13:58:55 +0000

    Added functionality to allow users to authenticate via youtube and add videos to their playlists

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-loadRedditResults = (selectedTime) => {
+var _youtubeUrl = "";
+
+function loadRedditResults(selectedTime) {
     var timeSpan = selectedTime.innerText;
     var time = "";
     switch (timeSpan) {
@@ -6,56 +8,56 @@ loadRedditResults = (selectedTime) => {
             time = "hour";
             break;
         case "Last 24 Hours":
-            time="day";
+            time = "day";
             break;
         case "Last Week":
-            time="week";
-        break;
+            time = "week";
+            break;
         case "Last Month":
-            time="month";
-        break;
+            time = "month";
+            break;
         case "Last Year":
-            time="year";
-        break;            
+            time = "year";
+            break;
         case "All Time":
-            time="all";
-        break; 
+            time = "all";
+            break;
         default:
-            time="all";
+            time = "all";
             break;
     }
 
     var url = "https://www.reddit.com/r/listentothis/top.json?t=" + time + "&limit=5000";
     document.getElementById("dt").innerText = timeSpan;
     fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then(function(json){
-        var wrapper = document.getElementById("results-data");
-        wrapper.innerHTML = "";
-        var topPosts = json.data.children;
-        console.log(topPosts);
-        for(let i = 0; i < topPosts.length; i++){
-            var currentPost = topPosts[i];
-            if(currentPost.kind != "t3") {
-                continue;
+        .then((response) => {
+            return response.json();
+        })
+        .then(function (json) {
+            var wrapper = document.getElementById("results-data");
+            wrapper.innerHTML = "";
+            var topPosts = json.data.children;
+            console.log(topPosts);
+            for (let i = 0; i < topPosts.length; i++) {
+                var currentPost = topPosts[i];
+                if (currentPost.kind != "t3") {
+                    continue;
+                }
+                currentPost = currentPost.data;
+                if (isValidPost(currentPost)) {
+                    generateInfoPanel(wrapper, currentPost);
+                }
             }
-            currentPost = currentPost.data;
-            if (isValidPost(currentPost)) {
-                generateInfoPanel(wrapper, currentPost);
-            }
-        }
-    });
+        });
 }
 
-setStreamingServiceColumn = (dropdownElement, index) => {
+function setStreamingServiceColumn(dropdownElement, index) {
     var selectedColumn = document.getElementById("column" + index);
     var stylingRulesName = applyStyling(dropdownElement.innerText);
     selectedColumn.classList.add(stylingRulesName);
 }
 
-generateStylingRules = (serviceName) => {
+function generateStylingRules(serviceName) {
     switch (serviceName.toLowerCase()) {
         case "youtube":
             return "youtubeStyle";
@@ -68,27 +70,132 @@ generateStylingRules = (serviceName) => {
     }
 }
 
-generateInfoPanel = (parentWrapper, redditPost) => {
+function generateInfoPanel(parentWrapper, redditPost) {
     var gridRow = document.createElement("div");
     gridRow.className = "row data-row";
-    
+
     var youtubePanel = generateYoutubePanel(redditPost);
     var redditPanel = generateRedditPanel(redditPost);
-    var spotifyPanel = generateSpotifyPanel();  
+    var spotifyPanel = generateSpotifyPanel();
+
+    var youtubePlaylistButtonColumn = generatePlaylistButton("YouTube", redditPost.url);
+    var spotifyPlaylistButtonColumn = generatePlaylistButton("Spotify", "");
 
     gridRow.appendChild(youtubePanel);
+    gridRow.appendChild(youtubePlaylistButtonColumn)
     gridRow.appendChild(redditPanel);
+    gridRow.appendChild(spotifyPlaylistButtonColumn)
     gridRow.appendChild(spotifyPanel);
 
     parentWrapper.appendChild(gridRow);
 
 }
 
-isValidPost = (post) => {
+function generatePlaylistButton(streamingService, youtubeUrl) {
+    var columnDiv = document.createElement("div");
+    columnDiv.className = "col-sm-1";
+
+    var button = document.createElement("button");
+    button.className = "btn btn-primary";
+    button.textContent = `Add to ${streamingService} playlist`;
+
+    button.onclick = function () {
+        displayPlaylistsModal(youtubeUrl);
+    }
+    
+    columnDiv.appendChild(button);
+
+    return columnDiv;
+}
+
+function generateRedditPanel(redditPost) {
+    var colDiv = document.createElement("div");
+    colDiv.className = "col-sm-4";
+
+    var youtubeUrl = redditPost.url;
+
+    var title = cleanRedditTitle(redditPost.title);
+    var titleElement = document.createElement("h4");
+    var titleText = document.createTextNode(title);
+    titleElement.appendChild(titleText);
+    colDiv.appendChild(titleElement);
+
+    return colDiv;
+}
+
+function displayPlaylistsModal(youtubeUrl) {
+    _youtubeUrl = youtubeUrl;
+    var wrapper = document.getElementById("youtubePlaylistsBody");
+    wrapper.innerHTML = "";
+    var modalGridDiv = document.createElement("div");
+    modalGridDiv.className = "container-fluid";
+
+    _playLists.forEach(playlist => {
+        var playlistInfo = document.createElement("div");
+        playlistInfo.className = "row";
+        
+        var playlistTitleDiv = document.createElement("div");
+        playlistTitleDiv.className = "col-sm-9";
+        playlistTitleDiv.innerText = playlist.title;
+
+        var playlistButtonDiv = document.createElement("div");
+        playlistButtonDiv.className = "col-sm-3";
+        
+        var addToPlaylistButton = document.createElement("button");
+        addToPlaylistButton.className = "btn btn-secondary";
+        addToPlaylistButton.innerText = "Add Here";
+        addToPlaylistButton.onclick = function () {
+            addVideoToPlaylist(playlist, youtubeUrl);   
+        }
+
+        playlistButtonDiv.appendChild(addToPlaylistButton);
+        playlistInfo.appendChild(playlistTitleDiv);
+        playlistInfo.appendChild(playlistButtonDiv);
+
+
+        modalGridDiv.appendChild(playlistInfo);
+    });
+
+    wrapper.appendChild(modalGridDiv);
+
+    $('#youtubePlaylistsModal').modal('show');
+}
+
+
+function generateYoutubePanel(redditPost) {
+    var colDiv = document.createElement("div");
+    colDiv.className = "col-sm-3";
+
+    var iFrameElement = document.createElement("iframe");
+
+    var youtubeSrc = redditPost.url.replace("watch?v=", "embed/");
+    iFrameElement.setAttribute("src", youtubeSrc);
+    iFrameElement.style.height = "100%";
+    iFrameElement.style.width = "75%";
+
+    colDiv.appendChild(iFrameElement);
+
+    return colDiv;
+}
+
+function generateSpotifyPanel() {
+    var colDiv = document.createElement("div");
+    colDiv.className = "col-sm-3";
+
+    var titleElement = document.createElement("h4");
+    var titleText = document.createTextNode("SPOTIFY HERE");
+    titleElement.appendChild(titleText);
+
+    colDiv.appendChild(titleElement);
+
+    return colDiv;
+}
+
+function isValidPost(post) {
     var title = post.title.toLowerCase();
     var validTitle = !title.includes("discussion") && !title.includes("playlist");
 
-    if(!post.url.toLowerCase().includes("youtube.com")) {
+    if (!post.url.toLowerCase().includes("youtube.com")) {
         return false;
     }
 
@@ -98,73 +205,16 @@ isValidPost = (post) => {
     return validFlair && validTitle && validArchive;
 }
 
-isValidFlair = (flair) => {
+function isValidFlair(flair) {
     if (flair == null) {
         return false;
     } else {
         var flairText = flair.toLowerCase();
-        return !flairText.includes("discussion") && 
-        !flairText.includes("playlist");
+        return !flairText.includes("discussion") &&
+            !flairText.includes("playlist");
     }
 }
 
-cleanRedditTitle = (redditTitle) => {
+function cleanRedditTitle(redditTitle) {
     return redditTitle.replace(/\(.*?\)|\[.*?\]/g, "").replace("--", "-");
-}
-
-generateRedditPanel = (redditPost) => {
-    var colDiv = document.createElement("div");
-    colDiv.className = "col-sm-4";
-
-    var youtubeUrl = redditPost.url;
-
-    var title = cleanRedditTitle(redditPost.title);
-    var titleElement = document.createElement("h4");
-    var titleText = document.createTextNode(title);
-    titleElement.appendChild(titleText); 
-
-    var addToPlaylistButton = document.createElement("button");
-    addToPlaylistButton.innerText = "Add To Playlist";
-    addToPlaylistButton.onclick = addToYoutubePlaylist(youtubeUrl);
-
-    colDiv.appendChild(titleElement);
-
-    return colDiv;
-}
-
-addToYoutubePlaylist = (url) => {
-    displayPlaylistsModal();
-}
-
-displayPlaylistsModal = () => {
-    listUserPlaylists();
-}
-
-generateYoutubePanel = (redditPost) => {
-    var colDiv = document.createElement("div");
-    colDiv.className = "col-sm-4";
-
-    var iFrameElement = document.createElement("iframe");
-   
-    var youtubeSrc = redditPost.url.replace("watch?v=" , "embed/");
-    iFrameElement.setAttribute("src", youtubeSrc);
-    iFrameElement.style.height = "100%";
-    iFrameElement.style.width = "75%";
-
-    colDiv.appendChild(iFrameElement);
-    
-    return colDiv;
-}
-
-generateSpotifyPanel = () => {
-    var colDiv = document.createElement("div");
-    colDiv.className = "col-sm-4";
-
-    var titleElement = document.createElement("h4");
-    var titleText = document.createTextNode("SPOTIFY HERE");
-    titleElement.appendChild(titleText); 
-
-    colDiv.appendChild(titleElement);
-    
-    return colDiv;
 }
--- youtube.js
+++ youtube.js
@@ -1,33 +1,59 @@
-var isAuthorised = false;
+var _isAuthorised = false;
+var _isLoaded = false;
+var _playLists = [];
 
 function authenticate() {
     return gapi.auth2.getAuthInstance()
         .signIn({
-            scope: "https://www.googleapis.com/auth/youtube.readonly"
+            scope: "https://www.googleapis.com/auth/youtube.force-ssl"
         })
         .then(function () {
                 console.log("Sign-in successful");
-                isAuthorised = true;
+                _isAuthorised = true;
             },
             function (err) {
                 console.error("Error signing in", err);
             });
 }
 
+function storePlaylists() {
+    if (!_isAuthorised || !_isLoaded) {
+        return;
+    }
+
+    return gapi.client.youtube.playlists.list({
+            "part": "snippet.contentDetails",
+            "maxResults": 25,
+            "mine": true
+        })
+        .then(res => {
+            console.log(res.result);
+        })
+}
+
+function displayPlaylists() {
+    _playLists.forEach(playlist => {
+        console.log(playlist);
+    })
+}
+
 function loadClient() {
     gapi.client.setApiKey("");
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function () {
+                _isLoaded = true;
                 console.log("GAPI client loaded for API");
+                console.log("Loading user's playlists");
+                fetchUsersPlaylists();
             },
             function (err) {
                 console.error("Error loading GAPI client for API", err);
             });
 }
 // Make sure the client is loaded and sign-in is complete before calling this method.
-function listUserPlaylists() {
+function fetchUsersPlaylists() {
 
-    if(!isAuthorised) {
+    if (!_isAuthorised || !_isLoaded) {
         return;
     }
 
@@ -38,11 +64,55 @@ function listUserPlaylists() {
         })
         .then(function (response) {
                 // Handle the results here (response.result has the parsed body).
-                console.log("Playlists -> ", response.result);
+                generateListOfPlaylists(response.result);
             },
             function (err) {
                 console.error("Execute error", err);
             });
+}
+
+function generateListOfPlaylists(playlistJson) {
+    var playlists = playlistJson.items;
+    _playLists = [];
+    playlists.forEach(playlist => {
+        var details = {
+            title: playlist.snippet.title,
+            id: playlist.id
+        };
+        _playLists.push(details);
+    });
+}
+
+function addVideoToPlaylist(playlist, url) {
+    if (!_isAuthorised || !_isLoaded) {
+        console.log("Not loaded/auth");
+        return;
+    }
+
+    var urlSegments = url.split("watch?v=");
+    var videoId = urlSegments[urlSegments.length - 1];
+
+    return gapi.client.youtube.playlistItems.insert({
+            "part": "snippet",
+            "resource": {
+                "snippet": {
+                    "playlistId": playlist.id,
+                    "position": 0,
+                    "resourceId": {
+                        "kind": "youtube#video",
+                        "videoId": videoId
+                    }
+                }
+            }
+        })
+        .then(function (response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+            },
+            function (err) {
+                console.error("Execute error", err);
+            });
+
 }
 
 function initYoutubeAuth() {
@@ -52,7 +122,7 @@ function initYoutubeAuth() {
                 client_id: ""
             });
         });
-        
+
     } catch (error) {
         console.log(error)
     }
